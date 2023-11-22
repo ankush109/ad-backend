306372a93058adb765426dc943dee8f041587ad6
Ankush Banerjee <banerjeeankush184@gmail.com>
2023-11-22 18:25:27 +0530

    made some changes

--- config/database.js
+++ config/database.js
@@ -1,12 +1,13 @@
 const { default: mongoose } = require("mongoose");
 
+const connectdb = () => {
+  mongoose
+    .connect(
+      "mongodb+srv://nila_boy:nila_boy@cluster0.ssn1p3g.mongodb.net/AdHub?retryWrites=true&w=majority"
+    )
+    .then((data) => {
+      console.log("mongo db connected with server ");
+    });
+};
 
-
-const connectdb=()=>{
-    mongoose.connect('mongodb+srv://nila_boy:nila_boy@cluster0.ssn1p3g.mongodb.net/AdHub?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true })
-.then((data)=>{
-    console.log("mongo db connected with server ",data.connection.host);
-})
-}
-
-module.exports=connectdb;
+module.exports = connectdb;
