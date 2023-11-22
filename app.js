d87d8d0ce0af36d0cf428bf106c5ca5d01e5705e
Ankush Banerjee <banerjeeankush184@gmail.com>
2023-11-22 19:17:01 +0530

    final changes

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const cors = require("cors");
+
 const errorMiddlewear = require("./middlewares/error");
 const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
