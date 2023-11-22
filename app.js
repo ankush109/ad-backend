fdb3ed9e6f8d9eee01e9839a3d60412ae2cac707
Ankush Banerjee <banerjeeankush184@gmail.com>
2023-11-22 19:14:41 +0530

    final changes

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const errorMiddlewear = require("./middlewares/error");
-const app = express();
 const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
 const fileupload = require("express-fileupload");
 const path = require("path");
-
+const app = express();
+const corsOptions = {
+  origin: ["https://ad-hub-frontend-995v.vercel.app", "http://localhost:3000"],
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
