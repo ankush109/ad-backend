28a5fd65c475fe16943c07e768b6ebe1917b86a2
Ankush Banerjee <banerjeeankush184@gmail.com>
2023-11-22 18:56:22 +0530

    final changes

--- app.js
+++ app.js
@@ -1,32 +1,29 @@
-const express =require ("express");
-const errorMiddlewear=require("./middlewares/error")
-const app=express();
-const cookieParser=require("cookie-parser")
-const bodyParser = require('body-parser')
-const fileupload=require('express-fileupload')
+const express = require("express");
+const dotenv = require("dotenv");
+const errorMiddlewear = require("./middlewares/error");
+const app = express();
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
+const fileupload = require("express-fileupload");
 const path = require("path");
 
-
+dotenv.config({ path: "config/config.env" });
 app.use(express.json());
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(fileupload());
 
-
-
 const product = require("./routes/ProductRoutes");
 
 app.use(product);
 
-const user=require("./routes/userRoutes");
+const user = require("./routes/userRoutes");
 
 app.use(user);
 
-const order=require("./routes/orderRoutes");
+const order = require("./routes/orderRoutes");
 
 app.use(order);
 
-
-
-app.use(errorMiddlewear)
+app.use(errorMiddlewear);
 module.exports = app;
